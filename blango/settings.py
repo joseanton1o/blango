666d7d5dc5fd565d099b454ee24631874bc6cf8e
JOSE PEREZ <jose-antonio.perez-lara@t-systems.com>
2026-01-19 11:55:03 +0000

    Finish crispy forms

--- blango/settings.py
+++ blango/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "blog",
 ]
 
