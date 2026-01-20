608567e530aa20a145faf0ac60cd7a981a951ee8
JOSE LARA <jose-antonio.perez-lara@t-systems.com>
2026-01-20 12:26:32 +0000

    Finish caching

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -3,6 +3,10 @@ from django.contrib.auth import get_user_model
 from django.utils.html import escape, format_html
 from blog.models import Post
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 user_model = get_user_model()
 register = template.Library()
 
@@ -32,6 +36,7 @@ def author_details(author, current_user):
 @register.inclusion_tag("blog/post-list.html")
 def recent_posts(post):
     posts = Post.objects.exclude(pk=post.pk)[:5]
+    logger.debug("Loaded %d recent posts for post %d", len(posts), post.pk)
     return {"title": "Recent Posts", "posts": posts}
 
 @register.simple_tag
