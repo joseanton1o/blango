439973fdbce5d156a85832ec0c00629b6e8e4330
JOSE LARA <jose-antonio.perez-lara@t-systems.com>
2026-01-26 15:21:31 +0000

    Finish permissions

--- blango/settings.py
+++ blango/settings.py
@@ -32,7 +32,10 @@ class Dev(Configuration):
           "rest_framework.authentication.BasicAuthentication",
           "rest_framework.authentication.SessionAuthentication",
           "rest_framework.authentication.TokenAuthentication",
-      ]
+      ],
+      "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+      ],
   }
   # SECURITY WARNING: don't run with debug turned on in production!
   DEBUG = values.BooleanValue(True)
