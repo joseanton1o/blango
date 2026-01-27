2389494cdf0779dcdf0f9b00578e3938910c38d0
JOSE LARA <jose-antonio.perez-lara@t-systems.com>
2026-01-27 09:18:08 +0000

    Finish viewsets and routers

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -4,7 +4,13 @@ from drf_yasg.views import get_schema_view
 import os
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views
-from blog.api.views import PostList, PostDetail, UserDetail
+from rest_framework.routers import DefaultRouter
+from blog.api.views import UserDetail, TagViewSet, PostViewSet
+
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Blango API",
@@ -14,9 +20,8 @@ schema_view = get_schema_view(
     url=f"https://{os.environ.get('CODIO_HOSTNAME')}-8000.codio.io/api/v1/",
     public=True,
 )
+
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
     re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
@@ -32,5 +37,6 @@ urlpatterns = [
 ]
 urlpatterns += [
     path("auth/", include("rest_framework.urls")),
-    path("token-auth/", views.obtain_auth_token)
+    path("token-auth/", views.obtain_auth_token),
+    path("", include(router.urls)),
 ]
