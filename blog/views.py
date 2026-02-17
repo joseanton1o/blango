b428f88a4925e89ef90d04836a93a275cecea194
JOSE LARA <jose-antonio.perez-lara@t-systems.com>
2026-02-17 09:09:53 +0000

    Finish JavaScript intro and fundamentals

--- blog/views.py
+++ blog/views.py
@@ -44,3 +44,6 @@ def post_detail(request, slug):
 
 def get_ip(request):
     return HttpResponse(request.META['REMOTE_ADDR'])
+
+def post_table(request):
+    return render(request, "blog/post-table.html")
